27180a55ffdd1a42f5dccc080f78d0565079a65e
Daniel Jiang <djiang11@illinois.edu>
2019-01-01 18:53:43 -0600

    Fix unsigned, signed, integer coversions

--- lc3.lisp
+++ lc3.lisp
@@ -37,31 +37,55 @@
   "Convert uint16 to an integer"
   (if (= (ldb (byte 1 15) u16) 0)
       u16
-      (1+ (logxor (- #xFFFF) u16))))
+      (logxor #xFFFF (1- (- u16)))))
+
+(defun int->u16 (int)
+  "Convert integer to a uint16"
+  (if (> int 0)
+      (wrap int)
+      (1+ (logxor #xFFFF (- int)))))
+
+(defun sign-extend (u16 bits)
+  "Sign extend to 16 bits"
+  (if (= (ldb (byte 1 (1- bits)) u16) 0)
+      u16
+      (1+ (logxor (ash #xFFFF (+ -16 bits)) (- u16)))))
 
 (defun update-conditional-register (u16)
-  "LC3 sets the conditional register with u16's sign when it writes a
-   register"
+  "LC3 sets the conditional register R9 with u16's sign when it writes
+   a register"
   (setf (aref *registers* 9) (position (signum (u16->int u16)) *flags*)))
+
+(defun mem (addr)
+  "Get block of memory at the address"
+  (aref *memory* addr))
+
+;; (defun (setf mem) (u16 addr)
+;;   "Set the block of memory "
+;;   (prog1 (setf (aref *memory* addr) u16)
+;;     (update-conditional-register u16)))
 
 (defun reg (idx)
   "Get the register at the index"
   (aref *registers* idx))
 
 (defun (setf reg) (u16 idx)
-  "Write to registers with SETF form"
-  (update-conditional-register u16)
-  (setf (aref *registers* idx) u16))
+  "Write to registers with SETF form and update the conditional register"
+  (prog1 (setf (aref *registers* idx) u16)
+    (update-conditional-register u16)))
 
 (defmacro with-spec (spec instr &body body)
   "Bind specification to instruction"
   `(let ,(loop for s in spec
                for name = (first s)
                for byte = (apply #'byte (rest s))
-               collect (list name (ldb byte instr)))
+               collect (list name `(ldb ',byte ,instr)))
      ,@body))
 
-(with-spec ((dr 3 9) (sr1 3 6) (mode 1 5) (sr2 3 0) (imm5 5 0)) #x1fff
-  (setf (reg dr)
-        (wrap (+ (aref *registers* sr1) (if (= mode 0) (aref *registers* sr2) imm5))))
-  (print (map 'vector #'u16->int *registers*)))
+;; ADD
+(funcall #'(lambda (instr)
+             (with-spec ((dr 3 9) (sr1 3 6) (mode 1 5) (sr2 3 0) (imm5 5 0)) instr
+               (setf (reg dr)
+                     (wrap (+ (aref *registers* sr1) (if (= mode 0) (aref *registers* sr2) (sign-extend imm5 5)))))
+               (print (map 'vector #'u16->int *registers*))))
+         #x1fff)
